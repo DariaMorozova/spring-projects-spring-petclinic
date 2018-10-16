78690720f1c14296a6d06485bacb4d00f9c015b1
Daria_Morozova <Daria_Morozova@epam.com>
2018-10-16 11:51:32 +0300

    fail test

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -89,7 +89,7 @@ public class ClinicServiceTests {
     public void shouldFindSingleOwnerWithPet() {
         Owner owner = this.owners.findById(1);
         assertThat(owner.getLastName()).startsWith("Franklin");
-        assertThat(owner.getPets().size()).isEqualTo(1);
+        assertThat(owner.getPets().size()).isEqualTo(2);
         assertThat(owner.getPets().get(0).getType()).isNotNull();
         assertThat(owner.getPets().get(0).getType().getName()).isEqualTo("cat");
     }
